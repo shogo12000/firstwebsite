e8baaed7c402c1e327de68ce19e73d1e6c79d219
sidney <shogo12000@hotmail.com>
2024-12-15 07:14:16 -0800

    create aside menu

---
+++ src/MainLayout.jsx
@@ -0,0 +1,79 @@
+import "./MainLayout.css";
+
+function MainLayout() {
+  return (
+    <>
+      <div className="layout">
+        <aside className="menu">
+          <nav>
+            <ul>
+              <li>
+                <i class="fa-solid fa-house"></i>
+                <p>Home</p>
+              </li>
+              <li>
+                <i class="fa-solid fa-link"></i>
+                <p>link1</p>
+              </li>
+              <li>
+                <i class="fa-solid fa-link"></i>
+                <p>link2</p>
+              </li>
+              <li>
+                <i class="fa-solid fa-link"></i>
+                <p>link3</p>
+              </li>
+              <li>
+                <i class="fa-solid fa-link"></i>
+                <p>link4</p>
+              </li>
+              <li>
+                <i class="fa-solid fa-right-to-bracket"></i>
+                <p>login</p>
+              </li>
+            </ul>
+          </nav>
+          <nav className="navBottom">
+            <ul>
+              <li>
+                <i class="fa-solid fa-address-book"></i>
+                <p>Contact</p>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <div className="main">
+          <div>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+            <p>um</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MainLayout;
